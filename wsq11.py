595ece132a6d64b906f800e0ad53cf1e5607fa4e
CharlieGDrummer <CharlieGDrummer@users.noreply.github.com>
2015-10-22 16:54:32 -0500

    Create wsq11.py

---
+++ wsq11.py
@@ -0,0 +1,77 @@
+#UNFINISHED    UNFINISHED    UNFINISHED
+
+natural = 0
+pali = 0
+lycherel = 0
+
+a = input ("Lower bound: ")
+b = input ("Upper bound: ")
+a = int(a)
+b = int(b)
+
+
+
+mainlist=[]
+for i in range (a,b+1):
+    mainlist.append(i)
+    
+lenght = len (mainlist)
+n = 0
+while (n != lenght):
+    mainlist[n] = str(mainlist[n])
+    n = n+1
+
+print (mainlist)
+
+
+for e in range (0,b):
+    
+    x = mainlist[e]
+    y = x[::-1]
+    
+    print ("Inside ",x)
+
+    if ( x == y):
+        natural = natural + 1
+    elif (x[-1] == "0"):
+        wasted =1
+        
+        
+        print ("wasted")
+        
+        
+        
+        
+    else:
+        
+        p = 0
+        while ((x != y) and (p != 30)):
+            x = int(x)
+            y = int(y)
+
+            x = x * y
+            
+            x = str(x)
+            y = x[::-1]
+            
+            
+            
+            
+            print ("working")
+            
+            
+            
+            
+
+            if (x == y):
+                pali = pali + 1
+
+            p = p+1
+        if ((x != y) and (p >= 30)):
+            lyrechel = lyrechel + 1
+
+                
+print ("Total numbers: ",lenght)
+print ("Natural: ",natural)
+print ("After: ",pali)
+print ("Lyrechel: ", lycherel)

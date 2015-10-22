af1b2ad173af00a5f122b962d95ae882e0b0ccab
CharlieGDrummer <CharlieGDrummer@users.noreply.github.com>
2015-10-22 17:14:07 -0500

    Update wsq11.py

--- wsq11.py
+++ wsq11.py
@@ -1,5 +1,3 @@
-#UNFINISHED    UNFINISHED    UNFINISHED
-
 natural = 0
 pali = 0
 lycherel = 0
@@ -8,8 +6,6 @@ a = input ("Lower bound: ")
 b = input ("Upper bound: ")
 a = int(a)
 b = int(b)
-
-
 
 mainlist=[]
 for i in range (a,b+1):
@@ -21,27 +17,14 @@ while (n != lenght):
     mainlist[n] = str(mainlist[n])
     n = n+1
 
-print (mainlist)
-
-
 for e in range (0,b):
-    
     x = mainlist[e]
     y = x[::-1]
     
-    print ("Inside ",x)
-
     if ( x == y):
         natural = natural + 1
     elif (x[-1] == "0"):
-        wasted =1
-        
-        
-        print ("wasted")
-        
-        
-        
-        
+        pali = pali + 1
     else:
         
         p = 0
@@ -49,29 +32,19 @@ for e in range (0,b):
             x = int(x)
             y = int(y)
 
-            x = x * y
+            x = x + y
             
             x = str(x)
             y = x[::-1]
             
-            
-            
-            
-            print ("working")
-            
-            
-            
-            
-
             if (x == y):
                 pali = pali + 1
-
             p = p+1
+            
         if ((x != y) and (p >= 30)):
-            lyrechel = lyrechel + 1
-
+            lycherel = lycherel + 1
                 
 print ("Total numbers: ",lenght)
-print ("Natural: ",natural)
-print ("After: ",pali)
-print ("Lyrechel: ", lycherel)
+print ("Natural palinndromes: ",natural)
+print ("Become palindromes: ",pali)
+print ("Lyrechel candidates: ", lycherel)

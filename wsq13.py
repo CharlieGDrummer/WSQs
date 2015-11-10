b15a09cfa2d7249a4d4e64c5f6be1f4ce5aba4b9
CharlieGDrummer <CharlieGDrummer@users.noreply.github.com>
2015-11-10 08:46:58 -0600

    Update wsq13.py

--- wsq13.py
+++ wsq13.py
@@ -1,14 +1,13 @@
-def bbsq(number)
-	close = number/2
-
-	first = (close + number / close)/2.0
-	r = (first + number/first)/2.0
-
-	while (r != ((number)**(1/2))):
-
-		r = (r + number/r)/2.0
-	
-	return (r)
+def bbsq(number):
+    start = number
+    
+    close = number/2
+    first = (close + number / close)/2.0
+    r = (first + number/first)/2.0
+    
+    while (r*r != start):
+        r = (r + number/r)/2.0
+    return (r)
 
 
 

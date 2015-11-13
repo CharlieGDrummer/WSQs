eeb2d94ad386a04d04571f41e0dc76dfc97ade8f
CharlieGDrummer <CharlieGDrummer@users.noreply.github.com>
2015-11-13 09:23:00 -0600

    Update wsq16.py

--- wsq16.py
+++ wsq16.py
@@ -1,11 +1,12 @@
 file = open("93cars.dat.txt",'r')
 
-
+count = 0
 for line in file:
-    x = line
-    manu = x[:14]
-    model = x[14:29]
-    midp = x[42:47]
-    cmpg = x[52:55]
-    hmpg = x[55:58]
-    print ("For the car ",manu," model ",model,", the miderange price is ", midp,", the city mpg is ",cmpg, ", and the highway mpg is ", hmpg)
+    if (count % 2 == 0):
+        manu = line[:14]
+        model = line[14:29]
+        midp = line[42:47]
+        cmpg = line[52:55]
+        hmpg = line[55:58]
+        print ("For the car ",manu," model ",model,", the miderange price is ", midp,", the city mpg is ",cmpg, ", and the highway mpg is ", hmpg)
+    count = count+1
